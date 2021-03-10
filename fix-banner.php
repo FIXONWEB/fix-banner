954d3b4c57b6730bf586130b8611700595fdf4bb
fixonweb <contato@fixonweb.com.br>
2021-03-10 10:59:09 -0300

    0.1.1

---
+++ fix-banner.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name:     Fix Banner
+ * Plugin URI:      https://github.com/fixonweb/fix-banner
+ * Description:     Exibir anuncios interno no seu web site
+ * Author:          FIXONWEB
+ * Author URI:      https://fixonweb.com.br
+ * Text Domain:     fix-banner
+ * Domain Path:     /languages
+ * Version:         0.1.1
+ *
+ * @package         Fix_Banner
+ */
+
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+require 'plugin-update-checker.php';
+$fix1608230887_url_update 	= 'https://github.com/fixonweb/fix-banner';
+$fix1608230887_slug 		= 'fix-banner/fix-banner';
+$fix1608230887_check 		= Puc_v4_Factory::buildUpdateChecker($fix1608230887_url_update,__FILE__,$fix1608230887_slug);
+
+
